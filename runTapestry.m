2aaf729fc1669c1d674202bd1b867387db338746
vpt101 <vishakhpt@yahoo.com>
2020-04-04 10:06:28 +0100

    loadData() integrated in runTapestry

--- runTapestry.m
+++ runTapestry.m
@@ -1,26 +1,6 @@
 pkg load io; % pkg install -forge io
 
-Config = Configuration();
-location = Config.('location');
-baseUrl = Config.('baseUrl');
-filteredCountries = Config.('defaultCountryList');
-protectorates = Config.('protectorates');
-
-filename = Config.('confirmedFilename');
-filePath = [location, '/', filename];
-url = [baseUrl, '/', filename];
-[dates, countryDataConfirmed, countryProvinceStruct] = processFile (protectorates, filePath, url);
-
-filename = Config.('recoveredFilename');
-filePath = [location, '/', filename];
-url = [baseUrl, '/', filename];
-[dates, countryDataRecovered, ~] = processFile (protectorates, filePath, url);
-
-filename = Config.('deathsFilename');
-filePath = [location, '/', filename];
-url = [baseUrl, '/', filename];
-[dates, countryDataDeaths, ~] = processFile (protectorates, filePath, url);
-
+[dates, countryDataConfirmed, countryDataRecovered, countryDataDeaths, countryProvinceStruct, filteredCountries] = loadData();
 
 subplot (3, 2, 1)
 plotBasicChart (filteredCountries, dates, countryDataConfirmed, "-o");
